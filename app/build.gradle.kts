16cd3c383851ccb884c5c7c6f0e1bc1ee0348e9f
SNfetchPLAYER <developer@stationnorth.media>
2026-09-23 19:16:37 +0200

    bump: release v1.0.0.27

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "net.sn.fetchplayer"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2026092303
-        versionName = "1.0.0.26 (2026.09.23-17:35)"
+        versionCode = 2026092304
+        versionName = "1.0.0.27 (2026.09.23-19:16)"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
